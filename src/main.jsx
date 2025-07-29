6ebbd9d07d241fefb1becb47b82ba5a37192f83f
SHAIK BUSHAR JAHA <busharjahashaik@gmail.com>
2025-07-29 15:42:05 +0530

    version2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Page1 from './page1.jsx'
 import Page2 from './page2.jsx'
 import Page3 from './Page3.jsx'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="frontendproject2"> 
   <Routes>
     <Route path="/"element = {<App/>}></Route>
     <Route path='/page1'element={<Page1/>}></Route>
